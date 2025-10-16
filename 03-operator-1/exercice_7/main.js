be44b857a50ec3f3472bfa91f7335802ea1e3699
Rachel fayila <fayilarachel72@gmail.com>
2025-10-16 22:04:34 +0100

    Ajout de l'exercice 7 JavaScript

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,21 @@
-// Exercice 7
-// Écrivez votre code ici
+let ageEnfant = 20;
+
+let agePere = ageEnfant * 2;
+
+let ageMere = agePere - 5;
+
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
+
+let ageOncle = agePere + 10;
+
+console.log("AGES DE LA FAMILLE");
+
+console.log("Enfant : " + ageEnfant + "ans");
+
+console.log("père : " + agePere + "ans");
+
+console.log("Mère : " + ageMere + "ans");
+
+console.log("grand-père : " + ageGrandPere + "ans");
+
+console.log("oncle : " + ageOncle + "ans")
